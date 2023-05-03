8fa24188ece43b2c9eab3f0f2fba58dd79618f86
Anish2919 <kumalanish98@gmail.com>
2023-05-03 11:48:06 +0545

    rename handlebars to hbs and default layout propery

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const PORT = 4000;
 const app = express(); 
 const {engine} = require('express-handlebars'); 
 
-app.set('view engine', 'handlebars'); 
+app.set('view engine', 'hbs'); 
 
-app.engine('handlebars', engine({
+app.engine('hbs', engine({
    layoutsDir: __dirname + '/views/layouts', 
+   extname: 'hbs', 
+   defaultLayout: 'mirror_index'
+   
 }))
 
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
-    res.render('main', {layout:'index'}); 
+    // res.render('main', {layout:'index'}); 
+    res.render('main'); 
 }); 
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`)); 
